8b57471337c67550d35490cd1492746679cc8e57
Rick South <rick@natamus.com>
2023-12-12 00:28:26 +0100

    Pumpkillager's Quest source code for 1.20.0.

---
+++ Common/src/main/java/com/natamus/pumpkillagersquest/data/Constants.java
@@ -0,0 +1,7 @@
+package com.natamus.pumpkillagersquest.data;
+
+import net.minecraft.client.Minecraft;
+
+public class Constants {
+    public static final Minecraft mc = Minecraft.getInstance();
+}
