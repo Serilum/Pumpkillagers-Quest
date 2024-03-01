e5fbd299f6ed6223a27131a4babf0acbbccd55e2
Rick South <rick@natamus.com>
2024-03-01 21:49:46 +0100

    (v3.7) The two quest books are invulnerable to explosions. [+2]

--- Common/src/main/java/com/natamus/pumpkillagersquest/cmds/CommandPumpkillager.java
+++ Common/src/main/java/com/natamus/pumpkillagersquest/cmds/CommandPumpkillager.java
@@ -1,37 +1,81 @@
 package com.natamus.pumpkillagersquest.cmds;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.natamus.collective.functions.ItemFunctions;
 import com.natamus.collective.functions.MessageFunctions;
+import com.natamus.pumpkillagersquest.util.Data;
 import com.natamus.pumpkillagersquest.util.Reference;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class CommandPumpkillager {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-		dispatcher.register(Commands.literal("pumpkillager").requires((iCommandSender) -> {
-			return iCommandSender.hasPermission(2);
-		}).then(Commands.literal("reset").then(Commands.argument("targets", EntityArgument.entities()).executes((command) -> {
-			CommandSourceStack source = command.getSource();
-			for (Entity entity : EntityArgument.getEntities(command, "targets")) {
-				List<String> tags = entity.getTags().stream().toList();
+		dispatcher.register(Commands.literal("pumpkillager").requires((iCommandSender) -> { return iCommandSender.hasPermission(2); })
+			.then(Commands.literal("reset")
+			.then(Commands.argument("targets", EntityArgument.entities())
+			.executes((command) -> {
+				CommandSourceStack source = command.getSource();
+				for (Entity entity : EntityArgument.getEntities(command, "targets")) {
+					List<String> tags = entity.getTags().stream().toList();
 
-				int deletedCount = 0;
-				for (String tag : tags) {
-					if (tag.contains(Reference.MOD_ID)) {
-						entity.getTags().remove(tag);
-						deletedCount += 1;
+					int deletedCount = 0;
+					for (String tag : tags) {
+						if (tag.contains(Reference.MOD_ID)) {
+							entity.getTags().remove(tag);
+							deletedCount += 1;
+						}
 					}
+					MessageFunctions.sendMessage(source, "Successfully deleted " + deletedCount + " pumpkillager tags for " + entity.getName().getString() + ".", ChatFormatting.DARK_GREEN);
 				}
-				MessageFunctions.sendMessage(source, "Successfully deleted " + deletedCount + " pumpkillager tags for " + entity.getName().getString() + ".", ChatFormatting.DARK_GREEN);
-			}
-			return 1;
-		}))));
+				return 1;
+			})))
+
+			.then(Commands.literal("book")
+			.then(Commands.literal("pumpkillagers-quest")
+			.executes((command) -> {
+				CommandSourceStack source = command.getSource();
+
+				try {
+					Player player = source.getPlayerOrException();
+
+					ItemFunctions.giveOrDropItemStack(player, Data.getQuestbook());
+
+					MessageFunctions.sendMessage(player, "You have been given the 'Pumpkillager's Quest' book.", ChatFormatting.DARK_GREEN);
+				}
+				catch (CommandSyntaxException ex) {
+					MessageFunctions.sendMessage(source, "This command can only be executed as a player in-game.", ChatFormatting.RED);
+					return 0;
+				}
+
+				return 1;
+			})))
+
+			.then(Commands.literal("book")
+			.then(Commands.literal("stopping-the-pumpkillager")
+			.executes((command) -> {
+				CommandSourceStack source = command.getSource();
+
+				try {
+					Player player = source.getPlayerOrException();
+
+					ItemFunctions.giveOrDropItemStack(player, Data.getStopPkbook());
+
+					MessageFunctions.sendMessage(player, "You have been given the 'Stopping the Pumpkillager' book.", ChatFormatting.DARK_GREEN);
+				}
+				catch (CommandSyntaxException ex) {
+					MessageFunctions.sendMessage(source, "This command can only be executed as a player in-game.", ChatFormatting.RED);
+					return 0;
+				}
+
+				return 1;
+			})))
+		);
     }
 }
