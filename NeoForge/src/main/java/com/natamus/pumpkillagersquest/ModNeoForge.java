3db1711c6c316b7a49f5f8e841c37db99f4e9e47
Rick South <rick@natamus.com>
2024-07-28 07:20:05 +0200

    (v4.4) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/pumpkillagersquest/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/pumpkillagersquest/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.pumpkillagersquest;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.pumpkillagersquest.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.pumpkillagersquest.neoforge.events.*;
 import com.natamus.pumpkillagersquest.neoforge.events.rendering.NeoForgeClientRenderEvent;
@@ -17,6 +18,10 @@ import net.neoforged.fml.loading.FMLEnvironment;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
