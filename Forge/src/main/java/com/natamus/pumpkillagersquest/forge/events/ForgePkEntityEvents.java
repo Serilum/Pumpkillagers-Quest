ff2f863b62259b8666792637c8b43c6f8ba16925
Rick South <rick@natamus.com>
2026-04-02 16:00:58 +0200

    Pumpkillager's Quest source code for 26.1.1.

---
+++ Forge/src/main/java/com/natamus/pumpkillagersquest/forge/events/ForgePkEntityEvents.java
@@ -0,0 +1,55 @@
+package com.natamus.pumpkillagersquest.forge.events;
+
+import com.natamus.pumpkillagersquest.events.PkEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.EntityJoinLevelEvent;
+import net.minecraftforge.event.entity.EntityLeaveLevelEvent;
+import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+import net.minecraftforge.fml.common.Mod;
+
+public class ForgePkEntityEvents {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgePkEntityEvents.class);
+	}
+
+    @SubscribeEvent
+    public static boolean onEntityJoin(EntityJoinLevelEvent e) {
+        Level level = e.getLevel();
+        if (level.isClientSide()) {
+            return false;
+        }
+
+        if (!PkEntityEvents.onEntityJoin(e.getEntity(), (ServerLevel)level)) {
+            return true;
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public static void onEntityLeave(EntityLeaveLevelEvent e) {
+        PkEntityEvents.onEntityLeave(e.getEntity(), e.getLevel());
+    }
+
+    @SubscribeEvent
+    public static void onItemPickup(EntityItemPickupEvent e) {
+        Player player = e.getEntity();
+        PkEntityEvents.onItemPickup(player.level(), player, e.getItem().getItem());
+    }
+
+    @SubscribeEvent
+    public static boolean onEntityHitByLightning(EntityStruckByLightningEvent e) {
+        Entity entity = e.getEntity();
+        if (!PkEntityEvents.onEntityHitByLightning(entity.level(), entity, e.getLightning())) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Forge/src/main/java/com/natamus/pumpkillagersquest/forge/events/ForgePkLivingEvents.java
@@ -0,0 +1,49 @@
+package com.natamus.pumpkillagersquest.forge.events;
+
+import com.natamus.pumpkillagersquest.events.PkLivingEvents;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+import net.minecraftforge.fml.common.Mod;
+
+public class ForgePkLivingEvents {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgePkLivingEvents.class);
+	}
+
+    @SubscribeEvent
+    public static boolean onDamagePumpkillager(LivingHurtEvent e) {
+        LivingEntity livingEntity = e.getEntity();
+
+        float damageAmount = e.getAmount();
+        float newAmount = PkLivingEvents.onDamagePumpkillager(livingEntity.level(), livingEntity, e.getSource(), damageAmount);
+
+        if (damageAmount != newAmount) {
+            e.setAmount(newAmount);
+
+            if (newAmount == 0F) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public static boolean onLivingDeath(LivingDeathEvent e) {
+        if (!PkLivingEvents.onLivingDeath(e.getEntity(), e.getSource(), 0F)) {
+            return true;
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public static void onEntityItemDrop(LivingDropsEvent e) {
+        LivingEntity livingEntity = e.getEntity();
+        PkLivingEvents.onEntityItemDrop(livingEntity.level(), livingEntity, e.getSource());
+    }
+}
