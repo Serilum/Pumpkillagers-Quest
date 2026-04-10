02a40f1c895f26c66caa948466f7b995c99e4fe8
Rick South <rick@natamus.com>
2026-04-10 02:47:20 +0200

    Pumpkillager's Quest source code for 26.1.2.

---
+++ Forge/src/main/java/com/natamus/pumpkillagersquest/ModForge.java
@@ -0,0 +1,56 @@
+package com.natamus.pumpkillagersquest;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
+import com.natamus.pumpkillagersquest.forge.config.IntegrateForgeConfig;
+import com.natamus.pumpkillagersquest.forge.events.*;
+import com.natamus.pumpkillagersquest.forge.events.rendering.ForgeClientRenderEvent;
+import com.natamus.pumpkillagersquest.util.Reference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge(FMLJavaModLoadingContext modLoadingContext) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
+		BusGroup busGroup = modLoadingContext.getModBusGroup();
+		FMLLoadCompleteEvent.getBus(busGroup).addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(modLoadingContext);
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		ForgePkAttackEvents.registerEventsInBus();
+		ForgePkBlockEvents.registerEventsInBus();
+		ForgePkEntityEvents.registerEventsInBus();
+		ForgePkLivingEvents.registerEventsInBus();
+		ForgePkOtherEvents.registerEventsInBus();
+		ForgePkPlayerEvents.registerEventsInBus();
+		ForgePkTickEvents.registerEventsInBus();
+		ForgePkWorldEvents.registerEventsInBus();
+
+		if (FMLEnvironment.dist.equals(Dist.CLIENT)) {
+			ForgeClientRenderEvent.registerEventsInBus();
+			ForgePkSoundEvents.registerEventsInBus();
+		}
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
